8618eeb45542338002ebe67ba034268403fe10cf
aayush-sinha <aayush_sinha@icloud.com>
2020-05-06 03:30:10 +0530

    minnor

--- app.js
+++ app.js
@@ -22,25 +22,25 @@ mongoose.connect('mongodb+srv://skylivingweb:Felix2020@@cluster0-jgx9s.mongodb.n
 }).catch(err => {
   console.log('ERROR:', err.message);
 });
-var roomSchema = new mongoose.Schema({
-  propName: String,
-  propLocation: String,
-  propAddress: String,
-  propSize: Number,
-  propLat: Number,
-  propLong: Number,
-  propRooms: Number,
-  propOccupancy: Array,
-  propCooling: Array,
-  propPrice: Array,
-  propImgExt: String,
-  propImg1: String,
-  propImag2: String,
-  propImg3: String,
-  propFeatures: Array,
-  propDate: { type: Date, default: Date.now },
-});
-var Room = mongoose.model("Room", roomSchema);
+// var roomSchema = new mongoose.Schema({
+//   propName: String,
+//   propLocation: String,
+//   propAddress: String,
+//   propSize: Number,
+//   propLat: Number,
+//   propLong: Number,
+//   propRooms: Number,
+//   propOccupancy: Array,
+//   propCooling: Array,
+//   propPrice: Array,
+//   propImgExt: String,
+//   propImg1: String,
+//   propImag2: String,
+//   propImg3: String,
+//   propFeatures: Array,
+//   propDate: { type: Date, default: Date.now },
+// });
+// var Room = mongoose.model("Room", roomSchema);
 
 // Room.find({}, function (err, room) {
 //   if (err) {

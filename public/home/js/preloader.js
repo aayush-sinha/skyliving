e3123a3ce0e6d9de1a63cfba5f4dcba1cc4324d3
aayush-sinha <aayush_sinha@icloud.com>
2020-05-09 23:32:27 +0530

    updated preloader and stuff

--- public/home/js/preloader.js
+++ public/home/js/preloader.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    //Preloader
-    preloaderFadeOutTime = 500;
-    function hidePreloader() {
-        var preloader = $('.loading-screen');
-        preloader.fadeOut(preloaderFadeOutTime);
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loading-screen").style.visibility = "visible";
+    } else {
+        document.querySelector(".loading-screen").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
     }
-    hidePreloader();
-});
+}; 
